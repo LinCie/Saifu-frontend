c78541311cd15cda3f3a459330108613f7456166
LinCie <aldiofernanda@protonmail.com>
2024-07-28 00:14:16 +0700

    added homepage

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,4 +1,5 @@
 import { Root } from "@/components";
+import { HomePage } from "@/pages";
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -6,7 +7,11 @@ import {
 } from "react-router-dom";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<Root />}></Route>),
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route path="/" element={<HomePage />} index />
+    </Route>,
+  ),
 );
 
 export default router;
