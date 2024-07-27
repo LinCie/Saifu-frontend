fa902769b558f5ec9c912f109eb0b2673aed3452
LinCie <aldiofernanda@protonmail.com>
2024-07-27 20:46:16 +0700

    remove user ability to select

--- src/components/Root/RootHeader.tsx
+++ src/components/Root/RootHeader.tsx
@@ -4,7 +4,7 @@ export function RootHeader() {
   return (
     <header className="bg-primary-900/70 fixed inset-x-0 top-0 z-[100] px-3 py-2 text-white backdrop-blur-sm md:px-4 md:py-3">
       <div className="flex items-center">
-        <div className="flex items-center gap-1">
+        <div className="flex items-center gap-1 select-none">
           <WalletIcon className="size-8 md:size-9" />
           <div className="text-xl font-bold">Open Wallet</div>
         </div>
