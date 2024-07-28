039f89725853c83b09940c260c9b6fe4eb4d0558
LinCie <aldiofernanda@protonmail.com>
2024-07-28 19:33:05 +0700

    fix min height

--- src/components/Root/Root.tsx
+++ src/components/Root/Root.tsx
@@ -5,7 +5,7 @@ export function Root() {
   return (
     <>
       <RootHeader />
-      <main className="min-h-screen bg-slate-100 dark:bg-gray-700">
+      <main className="min-h-screen bg-slate-100 px-2 dark:bg-gray-700">
         <Outlet />
       </main>
     </>
