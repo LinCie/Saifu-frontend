556e40bb24b50108857755feef9f5b9db06e896a
LinCie <aldiofernanda@protonmail.com>
2024-08-01 22:14:49 +0700

    add instance interceptors

--- src/services/instance.ts
+++ src/services/instance.ts
@@ -1,5 +1,20 @@
 import axios from "axios";
+import Cookies from "js-cookie";
+import { refresh } from "./auth";
 
-export const instanceV1 = axios.create({
+const instanceV1 = axios.create({
   baseURL: "/api/v1/",
 });
+
+instanceV1.interceptors.request.use(async (config) => {
+  let accessToken = Cookies.get("access_token");
+  if (!accessToken) {
+    accessToken = await refresh();
+  }
+
+  config.headers.Authorization = `Bearer ${accessToken}`;
+
+  return config;
+});
+
+export { instanceV1 };
