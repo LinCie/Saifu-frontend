04ef242b0f8592442a3bc74ca68335cfd8e5cb58
LinCie <aldiofernanda@protonmail.com>
2024-08-01 22:05:53 +0700

    added refresh service

---
+++ src/services/auth/refresh.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+
+interface RefreshResponse {
+  access_token: string;
+}
+
+export async function refresh() {
+  const refreshToken = Cookies.get("refresh_token");
+  if (!refreshToken) {
+    throw new Error("refresh token does not exist");
+  }
+
+  const response = await axios.post<RefreshResponse>("/api/v1/auth/refresh", {
+    refresh_token: refreshToken,
+  });
+
+  return response.data;
+}
