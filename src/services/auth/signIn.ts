f5c72ad78eb1ba6d791b138afe15ad093fdae8fd
LinCie <aldiofernanda@protonmail.com>
2024-08-07 18:25:03 +0700

    update signin response

--- src/services/auth/signIn.ts
+++ src/services/auth/signIn.ts
@@ -1,12 +1,14 @@
+import { User } from "@/entities";
 import { instanceV1 } from "../instances";
 
-interface SignInResponse {
+interface ISignInResponse {
+  user: User;
   access_token: string;
   refresh_token: string;
 }
 
 export async function signIn(username: string, password: string) {
-  const response = await instanceV1.post<SignInResponse>("auth/signin", {
+  const response = await instanceV1.post<ISignInResponse>("auth/signin", {
     username,
     password,
   });
