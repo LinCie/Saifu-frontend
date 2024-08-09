0a567dad6ef3f14a47cce831cdbe0cfe39465f6f
LinCie <aldiofernanda@protonmail.com>
2024-08-09 21:42:07 +0700

    change site logo to link

--- src/components/Root/RootHeader.tsx
+++ src/components/Root/RootHeader.tsx
@@ -1,14 +1,15 @@
 import { Bars3Icon, WalletIcon } from "@heroicons/react/24/outline";
 import { ThemeToggle } from "../Theme/ThemeToggle";
+import { Link } from "react-router-dom";
 
 export function RootHeader() {
   return (
-    <header className="bg-primary-900/70 fixed inset-x-0 top-0 z-[100] px-3 py-2 text-white backdrop-blur md:px-4 md:py-3 dark:bg-gray-800/70">
+    <header className="fixed inset-x-0 top-0 z-[100] bg-primary-900/70 px-3 py-2 text-white backdrop-blur md:px-4 md:py-3 dark:bg-gray-800/70">
       <div className="flex items-center">
-        <div className="flex select-none items-center gap-1">
+        <Link to="/" className="flex select-none items-center gap-1">
           <WalletIcon className="size-8 md:size-9" />
           <div className="text-xl font-bold">Open Wallet</div>
-        </div>
+        </Link>
         <div className="flex-1"></div>
         <span className="hidden md:block">
           <ThemeToggle />
